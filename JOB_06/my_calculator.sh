144d81ed973868039165119ebc3826b96bd2fc04
roger-latiere <roger.latiere@laplateforme.io>
2022-09-28 19:32:37 +0200

    Calculator update dans JOB_06

--- JOB_06/my_calculator.sh
+++ JOB_06/my_calculator.sh
@@ -7,12 +7,19 @@ then
 elif [ $2 = "-" ]
 then
 	result=$(( $1 - $3 ))
-# Pour la multiplication il faudra utiliser "x" dans la condition car "*" ne marche pas. Pour le résultat ça sera "*"
+	echo "Une soustraction, tel est votre choix"
+# Pour la multiplication il faudra utiliser "x" dans la condition car "*" ne marche pas. Pour avoir le résultat ça sera "*"
 elif [ $2 = "x" ]
 then
 	result=$(( $1 * $3 ))
+	echo "La multiplication, j'aime ça"
 elif [ $2 = "/" ]
 then
 	result=$(( $1 / $3 ))
+	echo "La division pour aller plus loin :)"
+	
+else 
+	echo "Je ne comprends pas, écrire convenablement votre opération sous la forme :
+	x + y par exemple, respectez bien les espaces"
 fi
 echo "Result : $result"
